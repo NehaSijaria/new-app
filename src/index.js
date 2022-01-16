9015f9e45e441e9d39bede29b4a94c1ee6d5b9b1
Neha <nehasijaria@gmail.com>
2022-01-15 20:42:11 -0500

    render comp  multiple ties

--- src/index.js
+++ src/index.js
@@ -3,21 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const MyComponent = (props) => { {
-  return (
-    <>
-      <h5>Calling component</h5>
-      <p>{props.compTitle}</p> 
-    </>
-  )
+const MyComponent = (props) => { 
+  {
+    return (
+      <>
+        <h5>Calling component</h5>
+        <p>{props.compTitle}</p> 
+      </>
+    )
+  }
 }
- 
+
+const Welcome = (props) => { 
+  {
+    return (
+      <>
+        <h5>Welcome component</h5>
+        <p>Welcome: {props.username}</p> 
+      </>
+    )
+  }
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <MyComponent compTitle = "Title: MyComponent"/>
+    <MyComponent compTitle = "Title:MyComponent"/>
     <App />
+    <Welcome username="Sam" />
+    <Welcome username="Samyi"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
