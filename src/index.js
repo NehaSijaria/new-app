6b69c768f48e2c4939b68a08f88ec04e652f6659
Neha <nehasijaria@gmail.com>
2022-01-15 20:18:30 -0500

    create fn and call

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const MyComponent = () => { {
+  return (
+    <p>Calling MyComponent</p>
+  )
+}
+ 
+}
+
 ReactDOM.render(
   <React.StrictMode>
+    <MyComponent />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
